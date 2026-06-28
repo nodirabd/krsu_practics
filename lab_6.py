1d1609c9c87eb4b9d802a21d5e4319e10a6a8b90
nodiraabd <abdurakhmanovanodirabegim@gmail.com>
2026-06-29 01:12:30 +0600

    lab 6

--- lab_6.py
+++ lab_6.py
@@ -1,49 +1,45 @@
-number= int(input("Введите номер задачи(1-4) "))
-if number == 1: 
-    n = int(input("Введите длину массива N: "))
+from random import *
 
-    x = []
+number = int(input("Type the num of the ex: "))
+
+if number == 1:
+    n = int(input("Size of the array: "))
+    ar = []
+
     for i in range(n):
-        print("Введите", i, "элемент:")
-        x.append(int(input()))
+        num= int(input(f'type {i+1} element: '))
+        ar.append(num)
 
+    print(f'max number of the array is {max(ar)}')
+    print(f'original : {ar}  reversed : {ar[::-1]}')
 
-    max_element = max(x)
-
-    print("Исходный массив:", x)
-    print("Максимальный элемент:", max_element)
-    #  срез [::-1]
-    print("Массив в обратном порядке:", x[::-1])
 elif number == 2:
-    
-    a = [1.5, 0.0, 3.4, 0.0, 5.1, -2.0]
-    print("Исходный массив:", a)
+    n = int(input("Size of the array: "))
+    array = [round(uniform(0, 1), 1) for i in range(n)]
+    print("Сгенерированный массив:", array)
+    av = round(sum(array)/len(array),1)
 
+    for i in range(n):
+        if array[i]== 0.0:
+            i = av
 
-    sum_elements = sum(a)
-    count_elements = len(a)
-    sr_arifm = sum_elements / count_elements
-    print("Среднее арифметическое:", round(sr_arifm, 4))
-
-    for i in range(count_elements):
-        if a[i] == 0.0:
-            a[i] = sr_arifm
-
-    print("Полученный массив:", a)
-
+    print(f'average is {av}')
+    print(f'new array: {array}')
 
 elif number == 3:
-    stroka = input("Введите символьную строку (словесный текст): ")
+    stroka = input("type a text: ")
     words = stroka.split()
 
     if len(words) > 0:
-        print("Первое слово:", words[0])
+        print(f'first word is "{words[0]}"')
     else:
-        print("Строка пуста или состоит только из пробелов.")
+        print("there is no text or there were just enters")
+
 elif number == 4:
     lst = [9, -3, 5, 0, -3]
-   
     sum_abs = sum(abs(i) for i in lst)
 
-    print("Исходный список:", lst)
-    print("Сумма абсолютных значений его элементов:", sum_abs)
+    print("previous list:", lst)
+    print("sum of the abs values of the list:", sum_abs)
+else:
+    print("there is no other ex for this var")
